372f492ff3bf48bdeb2f09b585667dcaffaf127e
Johannes Enevoldsen <johannes.ne@gmail.com>
2022-01-24 15:44:42 +0100

    update demo

--- demo/make.jl
+++ demo/make.jl
@@ -1,3 +1,5 @@
 using Weave
 
-weave("$(@__DIR__)/tree_compartemt_model/tree_compartment_model.jmd", doctype = "github")
+weave("$(@__DIR__)/tree_compartemt_model/tree_compartment_model.jmd", 
+    doctype = "github",
+    out_path = "$(@__DIR__)/tree_compartemt_model/README.md")
