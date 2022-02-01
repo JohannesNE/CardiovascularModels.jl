a234186fed16ccfb6dbbfb06c9eba567f19b281b
Johannes Enevoldsen <johannes.ne@gmail.com>
2022-02-01 10:07:56 +0100

    update three compartment test

--- test/test-three-compartment.jl
+++ test/test-three-compartment.jl
@@ -1,37 +1,52 @@
+using OrdinaryDiffEq, ModelingToolkit, CardiovascularModels
+
+@variables t
+D = Differential(t)
+
 #left Ventricle
 @named l_ventricle = Ventricle(Ees = 200e6,
-Vd = 0., V0 = 0., λ = 33e3, P0 = 10.,
-R_out = 6e6)
+    Vd = 0., V0 = 0., λ = 33e3, P0 = 10.)
 
 @named card_driver = Driver()
 
+@named aortic_valve = Valve(R = 6e6)
+
 # Aorta
-@named aorta = Vessel(Ees = 200e6, Vd = 100e-6, R_out = 50e6)
+@named aorta = Vessel(Ees = 200e6, Vd = 100e-6)
+
+@named systemic_resistance = Resistor(R = 50e6)
 
 eqs_driver = [
-l_ventricle.drv ~ card_driver.contraction
+    l_ventricle.drv ~ card_driver.contraction
 ]
 
-#@named vein = Const_Pressure(P = 1e3, R_out = 6e6, valve_out = true)
-@named vein = Vessel(Ees = 5e6, Vd = 500e-6, R_out = 10e6, valve_out = true)
+@named vein = Vessel(Ees = 5e6, Vd = 500e-6)
+@named mitral_valve = Valve(R = 10e6)
+
+
 
 eqs_con = [
-connect(vein.out, l_ventricle.in),
-connect(l_ventricle.out, aorta.in),
-connect(aorta.out, vein.in)
+    connect(vein.out, mitral_valve.in),
+    connect(mitral_valve.out, l_ventricle.in),
+    connect(l_ventricle.out, aortic_valve.in),
+    connect(aortic_valve.out, aorta.in),
+    connect(aorta.out, systemic_resistance.in),
+    connect(systemic_resistance.out, vein.in)
 ]
+
 
 eqs_comb = [eqs_driver; eqs_con]
 
 # ODE parameters
 volume_start = [l_ventricle.V => 1e-4, 
-aorta.V => 1.5e-4,
-vein.V => 2e-3]
+    aorta.V => 1.5e-4,
+    vein.V => 2e-3]
 time_span = (0.0, 10.0)
 
 @named hemo_sys = ODESystem(eqs_comb)
 
-@named connected = compose(hemo_sys, l_ventricle, aorta, vein, card_driver)
+@named connected = compose(hemo_sys, l_ventricle, card_driver, 
+                            aortic_valve, aorta, systemic_resistance, vein, mitral_valve)
 
 problem = ODEProblem(structural_simplify(connected), volume_start, time_span, [])
 
